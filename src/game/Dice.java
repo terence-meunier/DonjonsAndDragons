affc341ab5fb4758f7931840783495a4bac93e51
Térence MEUNIER <terence.meunier@gmail.com>
2021-04-29 16:09:52 +0200

    feat(Dice): add dice class with random method fixes #19

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,4 +1,19 @@
 package game;
 
 public class Dice {
+    private int face;
+
+    public Dice() {
+        this.face = 1;
+    }
+
+    public int throwDice() {
+        Double randNumber = Math.random()*6;
+        this.face = randNumber.intValue();
+        return this.face;
+    }
+
+    public int getFace() {
+        return this.face;
+    }
 }
