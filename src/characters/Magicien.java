862e1b807e650ff2794db460e4577ac0a10f3220
Térence <terence.meunier@gmail.com>
2021-04-30 15:53:12 +0200

    feat(Docs): Add javadoc comments for Magicien Class #30

--- src/characters/Magicien.java
+++ src/characters/Magicien.java
@@ -2,36 +2,88 @@ package characters;
 
 import stuffs.Spell;
 
+/**
+ *
+ * Classe Magicien qui permet de créer un objet de type Guerrier, hérite de la classe abstraite Character
+ *
+ * @see Character
+ *
+ * @author Térence MEUNIER
+ * @version 1.0
+ */
 public class Magicien extends Character {
 	private Spell spell;
 	private String filter;
 
+	/**
+	 * Constructeur vide
+	 */
 	public Magicien() {
 		this(null);
 	}
-	
+
+	/**
+	 * Constructeur avec un paramètre
+	 * @param name String
+	 *             Le nom du magicien
+	 */
 	public Magicien(String name) {
 		this(name, 3, 8);
 	}
-	
+
+	/**
+	 * Constructeur avec 3 paramètres
+	 * @param name String
+	 *             Le nom du magicien
+	 * @param lifeLevel int
+	 *             Les points de vie du magicien
+	 * @param strongLevel int
+	 *             Le niveau d'attaque du magicien
+	 */
 	public Magicien(String name, int lifeLevel, int strongLevel) {
 		super(name, lifeLevel, strongLevel);
 		this.spell = null;
 		this.filter = "Soin";
 	}
 
+	/**
+	 * Getter Récupère le sort du magicien sous la forme d'un objet de type Spell
+	 *
+	 * @see Spell
+	 *
+	 * @return Spell spell
+	 */
 	public Spell getSpell() {
 		return spell;
 	}
 
+	/**
+	 *
+	 * Setter affecter un nouveau sort au magicien
+	 *
+	 * @param spell
+	 * 		Le sort à affecté au magicien
+	 */
 	public void setSpell(Spell spell) {
 		this.spell = spell;
 	}
 
+	/**
+	 * Getter permettant de récupèrer le filtre du magicien
+	 * @return String filter
+	 * 		Le filtre du magicien sous la forme d'un objet String
+	 *
+	 */
 	public String getFilter() {
 		return filter;
 	}
 
+	/**
+	 * Setter permettnat d'affecter un filtre au magicien
+	 * @param filter
+	 * 		Le filtre à affecté au magicien
+	 *
+	 */
 	public void setFilter(String filter) {
 		this.filter = filter;
 	}
