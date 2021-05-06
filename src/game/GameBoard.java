c34b329b25f20b548d3a48d6176844964370cfdb
Térence MEUNIER <terence.meunier@gmail.com>
2021-05-06 10:55:45 +0200

    feat(Bonus Weapons): add Arc in Weapon #47

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -28,7 +28,7 @@ public class GameBoard {
                 board.add(new Ennemy("Dragon", 15, 4));
             } else if (i == 10 || i == 20 || i == 25 || i == 32 || i == 35 || i == 36 || i == 37 || i == 40 || i == 44 || i == 47) {
                 board.add(new Ennemy("Sorcier", 9, 2));
-            } else if (i == 3 || i == 6 || i == 9 || i == 12 || i == 15 || i == 18 || i == 21 || i == 24 || i == 27 || i == 30) {
+            } else if (i == 3 || i == 6 || i == 9 || i == 12 || i == 15 || i == 18 || i == 21) {
                 board.add(new Ennemy("Gobelin", 6, 1));
             } else if (i == 2 || i == 11 || i == 5 || i == 22 || i == 38) {
                 board.add(new Weapon("Massue", 3));
@@ -46,6 +46,8 @@ public class GameBoard {
                 board.add(new Ennemy("Orc", 10, 6));
             } else if (i == 50 || i == 14 ||i == 29) {
                 board.add(new Ennemy("Bad spirit", 15, 5));
+            } else if (i == 24 || i == 27 || i == 30) {
+                board.add(new Weapon("Arc", 4));
             } else {
                 board.add(null);
             }
